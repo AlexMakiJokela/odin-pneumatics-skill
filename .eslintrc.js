29cfa59931ac2ac9e60bd9c2c494c8af4b0daec9
alexmakijokela <alex.makijokela@gmail.com>
2024-05-05 17:12:43 -0600

    rm concurrently

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,3 @@
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'spruce'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	root: true,
-  };
+	extends: 'spruce',
+}
